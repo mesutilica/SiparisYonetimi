7bc5c01caa6670994292629e6aa3d941d20436e2
mesutilica <mesut.ilica@gmail.com>
2022-09-20 22:01:17 +0300

    20-09-2022 Repository classındayız

--- SiparisYonetimi.Business/Managers/UserManager.cs
+++ SiparisYonetimi.Business/Managers/UserManager.cs
@@ -1,19 +1,39 @@
 ﻿using SiparisYonetimi.Data;
 using SiparisYonetimi.Entities;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SiparisYonetimi.Business.Managers
 {
-    public class UserManager
+    public class UserManager // klavyden f12 tuşu ile direk gelebiliriz
     {
-        DatabaseContext context = new DatabaseContext();
+        DatabaseContext context = new DatabaseContext(); // Repository Pattern
         public List<User> GetAll()
         {
             return context.Users.ToList();
+        }
+        public List<User> GetAll(string kelime)
+        {
+            return context.Users.Where(user => user.Name.Contains(kelime) || user.Surname.Contains(kelime)).ToList();
+        }
+        public int Add(User user)
+        {
+            context.Users.Add(user); // context e gelen user ı ekliyor
+            return context.SaveChanges(); // context deki değişiklikleri kaydet
+        }
+        public User Find(int id)
+        {
+            return context.Users.Find(id);
+        }
+        public int Update(User user)
+        {
+            context.Entry(user).State = System.Data.Entity.EntityState.Modified;
+            return context.SaveChanges();
+        }
+        public int Remove(User user)
+        {
+            context.Users.Remove(user);
+            return context.SaveChanges();
         }
     }
 }
---
+++ SiparisYonetimi.Business/Repositories/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace SiparisYonetimi.Business.Repositories
+{
+    public interface IRepository<T> // <T> kullanarak interface i generic hale getirdik. Buradaki T dışarıdan parametre olarak gönderilecek class ları(brand, category vb) simgeliyor
+    {
+        List<T> GetAll();
+        List<T> GetAll(Expression<Func<T, bool>> expression);
+        T Find(int id);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        int SaveChanges();
+    }
+}
