af52ea2f66e2403451bdf8e04f3b37b4976c0617
mesutilica <mesut.ilica@gmail.com>
2022-12-15 21:31:59 +0300

    15-12-2022 müşteri yönetimi ok

---
+++ SiparisYonetimi.MVCUI/Areas/Admin/Controllers/KullaniciYonetimiController.cs
@@ -0,0 +1,96 @@
+﻿using SiparisYonetimi.Business.Managers;
+using SiparisYonetimi.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SiparisYonetimi.MVCUI.Areas.Admin.Controllers
+{
+    public class KullaniciYonetimiController : Controller
+    {
+        UserManager manager = new UserManager();
+        // GET: Admin/KullaniciYonetimi
+        public ActionResult Index()
+        {
+            var model = manager.GetAll();
+            return View(model);
+        }
+
+        // GET: Admin/KullaniciYonetimi/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Admin/KullaniciYonetimi/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/KullaniciYonetimi/Create
+        [HttpPost]
+        public ActionResult Create(User user)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                manager.Add(user);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Admin/KullaniciYonetimi/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var model = manager.Find(id);
+            return View(model);
+        }
+
+        // POST: Admin/KullaniciYonetimi/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, User user)
+        {
+            try
+            {
+                // TODO: Add update logic here
+                manager.Update(user);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Admin/KullaniciYonetimi/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var model = manager.Find(id);
+            return View(model);
+        }
+
+        // POST: Admin/KullaniciYonetimi/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+                var model = manager.Find(id);
+                manager.Remove(model);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ SiparisYonetimi.MVCUI/Areas/Admin/Controllers/MusteriYonetimiController.cs
@@ -0,0 +1,101 @@
+﻿using SiparisYonetimi.Business.Managers;
+using SiparisYonetimi.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SiparisYonetimi.MVCUI.Areas.Admin.Controllers
+{
+    public class MusteriYonetimiController : Controller
+    {
+        CustomerManager manager = new CustomerManager();
+        // GET: Admin/MusteriYonetimi
+        public ActionResult Index()
+        {
+            var model = manager.GetAll();
+            return View(model);
+        }
+
+        // GET: Admin/MusteriYonetimi/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Admin/MusteriYonetimi/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/MusteriYonetimi/Create
+        [HttpPost]
+        public ActionResult Create(Customer customer)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                manager.Add(customer);
+                manager.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                ModelState.AddModelError("", "Hata Oluştu!");
+            }
+            return View(customer);
+        }
+
+        // GET: Admin/MusteriYonetimi/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var model = manager.Find(id);
+            return View(model);
+        }
+
+        // POST: Admin/MusteriYonetimi/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, Customer customer)
+        {
+            try
+            {
+                // TODO: Add update logic here
+                manager.Update(customer);
+                manager.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                ModelState.AddModelError("", "Hata Oluştu!");
+            }
+            return View(customer);
+        }
+
+        // GET: Admin/MusteriYonetimi/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var model = manager.Find(id);
+            return View(model);
+        }
+
+        // POST: Admin/MusteriYonetimi/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+                var customer = manager.Find(id);
+                manager.Delete(customer);
+                manager.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
